92d4e3bcc324bb4694871f03341a2ac4be71f882
Yalla Balaji <yallabalaji2001@gmail.com>
2025-02-10 03:26:30 +0530

    packge , config, global css and routes configured

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/LoginPage";
+import Dashboard from "./pages/Dashboard";
+import SignUpPage from './pages/SignUpPage';
+import RoutePlannerDashboard from './pages/PlannerDashboard';
+import HubRegisterPage from "./pages/HubRegistration";
+import HubViewPage from "./pages/HubViewPage";
+import GraphPage from "./pages/GraphPage";
+import routes from "./routes";
+import RouteRegistrationPage from "./pages/RouteRegistration";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Router>
+      <Routes>
+      <Route path={routes.login} element={<Login />} />
+      <Route path={routes.dashboard} element={<Dashboard />} />
+      <Route path={routes.signup} element={<SignUpPage />} /> 
+      <Route path={routes.routePlannerDashboard} element={<RoutePlannerDashboard />} />
+      <Route path={routes.hubRegistartion} element={<HubRegisterPage />} />
+      <Route path={routes.hubView} element={<HubViewPage />} />
+      <Route path={routes.graphPage} element={<GraphPage />} />
+      <Route path={routes.routeRegistration} element={<RouteRegistrationPage />} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App
+export default App;
---
+++ webapp/src/routes.ts
@@ -0,0 +1,15 @@
+const routes = {
+    home: "/",
+    login: "/login",
+    dashboard: "/dashboard",
+    profile: "/profile",
+    signup: "/register-user",  // Add a new route for SignUpPage
+    routePlannerDashboard : "/route-planner", // Add a new route for RoutePlannerDashboard
+    hubRegistartion : "/hub-register", // Add a new route for HubRegisterPage
+    hubView : "/view-hubs", // Add a new route for HubViewPage
+    graphPage : "/view-graphs", // Add a new route for GraphPage
+    routeRegistration : "/route-registration" // Add a new route for RouteRegistrationPage
+  };
+  
+  export default routes;
+  

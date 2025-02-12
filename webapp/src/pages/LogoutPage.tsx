cba4c235a644cfe4fc23b5430b04c22ab121601f
Yalla Balaji <yallabalaji2001@gmail.com>
2025-02-13 05:13:31 +0530

    naming convention build issue resolved

--- webapp/src/pages/LogoutPage.tsx
+++ webapp/src/pages/LogoutPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import styles from "../styles/Dashboard.module.css";
+import styles from "../styles/dashboard.module.css";
 
 const Logout = () => {
   const navigate = useNavigate();
